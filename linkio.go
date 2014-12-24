1ad3eb7ea2768c9ace62acdb9e455c68f1ac693a
Ian Kent <email@iankent.co.uk>
2014-12-24 11:41:36 +0000

    Add io.Writer support

--- linkio.go
+++ linkio.go
@@ -44,6 +44,13 @@ type LinkReader struct {
 	link *Link
 }
 
+// A LinkWriter wraps an io.Writer, simulating writer to a
+// shared access link with a fixed maximum speed.
+type LinkWriter struct {
+	w    io.Writer
+	link *Link
+}
+
 // A Link serializes requests to sleep, simulating the way data travels
 // across a link which is running at a certain kbps (kilo = 1024).
 // Multiple LinkReaders can share a link (simulating multiple apps
@@ -51,6 +58,7 @@ type LinkReader struct {
 // by Go when scheduling reads from a contested blocking channel.
 type Link struct {
 	in    chan linkRequest
+	out   chan linkRequest
 	speed int64 // nanosec per bit
 }
 
@@ -68,10 +76,17 @@ func (link *Link) NewLinkReader(r io.Reader) (s *LinkReader) {
 	return
 }
 
+// NewLinkWriter returns a LinkWriter that writes bytes to r,
+// simulating that they arrived from a shared link.
+func (link *Link) NewLinkWriter(w io.Writer) (s *LinkWriter) {
+	s = &LinkWriter{w: w, link: link}
+	return
+}
+
 // NewLink returns a new Link running at kbps.
 func NewLink(throughput Throughput) (l *Link) {
 	// allow up to 100 outstanding requests
-	l = &Link{in: make(chan linkRequest, 100)}
+	l = &Link{in: make(chan linkRequest, 100), out: make(chan linkRequest, 100)}
 	l.SetThroughput(throughput)
 
 	// This goroutine serializes the requests. He could calculate
@@ -87,14 +102,22 @@ func NewLink(throughput Throughput) (l *Link) {
 			lr.done <- true
 		}
 	}()
+	go func() {
+		for lr := range l.out {
+			// bits * nanosec/bit = nano to wait
+			delay := time.Duration(int64(lr.bytes*8) * l.speed)
+			time.Sleep(delay)
+			lr.done <- true
+		}
+	}()
 
 	return
 }
 
 // SetThroughput sets the current link throughput
-func (l *Link) SetThroughput(throughput Throughput) {
-	// l.speed is stored in ns/bit
-	l.speed = 1e9 / int64(throughput)
+func (link *Link) SetThroughput(throughput Throughput) {
+	// link.speed is stored in ns/bit
+	link.speed = 1e9 / int64(throughput)
 }
 
 // why isn't this in package math? hmm.
@@ -113,6 +136,26 @@ func (l *LinkReader) Read(buf []byte) (n int, err error) {
 	// size.
 	toRead := min(len(buf), 1500)
 	n, err = l.r.Read(buf[0:toRead])
+	if err != nil {
+		return 0, err
+	}
+
+	// send in the request to sleep to the Link and sleep
+	lr := linkRequest{bytes: n, done: make(chan bool)}
+	l.link.in <- lr
+	_ = <-lr.done
+
+	return
+}
+
+// Satisfies interface io.Writer.
+func (l *LinkWriter) Write(buf []byte) (n int, err error) {
+	// Write small chunks at a time, even if they attempt more,
+	// preventing one LinkReader from saturating the simulated link.
+	// 1500 is the MTU for Ethernet, i.e. a likely maximum packet
+	// size.
+	toWrite := min(len(buf), 1500)
+	n, err = l.w.Write(buf[0:toWrite])
 	if err != nil {
 		return 0, err
 	}
--- linkio_test.go
+++ linkio_test.go
@@ -10,7 +10,7 @@ import (
 	"testing"
 )
 
-func TestOne(t *testing.T) {
+func TestReader(t *testing.T) {
 	// a dummy buffer full of zeros to send over the link
 	var y [1000]byte
 	buf := bytes.NewBuffer(y[:])
@@ -27,4 +27,27 @@ func TestOne(t *testing.T) {
 		}
 		t.Log("got", n, "bytes")
 	}
+}
+
+type fw struct {
+	buf []byte
+}
+
+func (f *fw) Write(buf []byte) (int, error) {
+	f.buf = append(f.buf, buf...)
+	return len(buf), nil
+}
+
+func TestWriter(t *testing.T) {
+	// a fake writer
+	w := &fw{}
+	// a dummy buffer full of zeros to send over the link
+	var y [1000]byte
+	lw := NewLink(Throughput(30) * KilobitPerSecond /* kbps */).NewLinkWriter(w)
+	n, err := lw.Write(y[:])
+	if err != nil {
+		t.Error("err ", err)
+	}
+	t.Log("wrote", n, "bytes")
+	t.Log("got", len(w.buf), "bytes")
 }
